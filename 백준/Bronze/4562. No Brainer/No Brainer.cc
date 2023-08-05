5cb9e03e3a2467cc41009fb2a6cefe44c3821648
김경준 <49617190+AzureSoda@users.noreply.github.com>
2023-08-05 22:03:47 +0900

    [Bronze III] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        int x, y;
+        cin >> x >> y;
+        if (x >= y) {
+            cout << "MMM BRAINS\n";
+        } else {
+            cout << "NO BRAINS\n";
+        }
+    }
+    return 0;
+}
