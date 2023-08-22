4f26287e53af2165be32f1bd008b9f2c080d41de
김경준 <49617190+AzureSoda@users.noreply.github.com>
2023-08-22 21:52:03 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int A, I;
+    std::cin >> A >> I;
+
+    int result = A * (I - 1) + 1;
+    std::cout << result << std::endl;
+
+    return 0;
+}
