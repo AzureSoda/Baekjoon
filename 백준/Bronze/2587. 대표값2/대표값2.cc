0e8d7a14a2aaae6538e287feed1d05820aa1a4a5
김경준 <49617190+AzureSoda@users.noreply.github.com>
2023-09-15 15:11:26 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int arr[5];
+    int sum = 0;
+
+    for (int i = 0; i < 5; ++i) {
+        std::cin >> arr[i];
+        sum += arr[i];
+    }
+
+    std::sort(arr, arr + 5);
+
+    std::cout << sum / 5 << '\n';
+    std::cout << arr[2] << '\n';
+
+    return 0;
+}
