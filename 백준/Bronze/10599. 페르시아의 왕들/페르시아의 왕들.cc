6d38f12f5fa1ee871e878c351ea5507375655470
김경준 <49617190+AzureSoda@users.noreply.github.com>
2023-09-20 21:54:50 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, c, d;
+    
+    while(true){
+        cin >> a >> b >> c >> d;
+        if(a == 0 && b == 0 && c == 0 && d == 0) break;
+        
+        cout << (c - b) << " " << (d - a) << '\n';
+    }
+    
+    return 0;
+}
