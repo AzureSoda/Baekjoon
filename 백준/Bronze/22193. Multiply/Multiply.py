6cc4581dcb3589c92003af08649d7a012e022523
김경준 <49617190+AzureSoda@users.noreply.github.com>
2023-11-22 14:33:10 +0900

    [Bronze V] Title: Multiply, Time: 160 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.py
+++ 백준/Bronze/22193. Multiply/Multiply.py
@@ -1,20 +1,5 @@
-def multiply(A, B):
-    C = [0] * (len(A) + len(B) + 1)
-    for i in range(len(A)):
-        for j in range(len(B)):
-            C[i+j] += A[i] * B[j]
-    for i in range(len(C) - 1):
-        if C[i] >= 10:
-            C[i+1] += C[i] // 10
-            C[i] %= 10
-    while len(C) > 1 and C[-1] == 0:
-        C.pop(-1)
-    return C
+N, M = map(int, input().split())
+A = int(input().strip())
+B = int(input().strip())
 
-len_A, len_B = map(int, input().split())
-A = list(map(int, reversed(input().strip())))
-B = list(map(int, reversed(input().strip())))
-
-C = multiply(A, B)
-for i in reversed(C):
-    print(i, end="")
+print(A*B)
