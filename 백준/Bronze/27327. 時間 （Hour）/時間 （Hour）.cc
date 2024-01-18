18f0e5112813f1b3af54800cec79992327b46683
김경준 <49617190+AzureSoda@users.noreply.github.com>
2024-01-18 10:53:45 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int X; // X를 저장할 변수 선언
+    std::cin >> X; // 사용자로부터 일수를 입력받음
+    std::cout << X * 24; // 입력받은 일수에 24를 곱하여 시간으로 변환하고 출력
+    return 0; // 프로그램 종료
+}
