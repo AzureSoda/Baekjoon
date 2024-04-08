4001a579dbc5dbc52b75949941e84a840f1d08df
김경준 <49617190+AzureSoda@users.noreply.github.com>
2024-04-08 16:23:21 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y; // 리ン고(X)와 미칸(Y)의 개수를 입력 받음
+    cout << X + Y + 3; // 리인고, 미칸, 그리고 바나나(3개)의 합을 출력
+    return 0;
+}
