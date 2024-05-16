6b3ca86d8500d00d8fbcea9ac51a0e243cfb20fb
김경준 <49617190+AzureSoda@users.noreply.github.com>
2024-05-16 18:53:15 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int totalCost = (A * B) + C;
+    cout << totalCost << endl;
+
+    return 0;
+}
